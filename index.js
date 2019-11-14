a617b8cc82b361dabc0c2c8055f9dad377ad071b
Teddy <megareda1998@gmail.com>
2019-11-14 15:42:55 +0100

    Commentaire + test mustache

--- index.js
+++ index.js
@@ -7,21 +7,33 @@ const nodeFetch     = require('node-fetch');
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+// Configuration d'express
 const app = express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'mustache')
     .engine('mustache', hogan.__express);
 
+// Grâce à Pupeteer, récupère les données de la page dans l'url et renvoie un objet Javascript
 async function getGbData(){
-	const dtUrl = 'https://deskthority.net/viewforum.php?f=50';
-    const browser = await puppeteer.launch();
+    const dtUrl = 'https://deskthority.net/viewforum.php?f=50';
+    
+    // Lance le browser Chromium ({headless: false} == on voit la page se lancer, passer à true pour que ça se joue en background)
+    const browser = await puppeteer.launch({headless: false});
+
+    // On créer un nouvel onglet
     const page = await browser.newPage();
+
+    // On accède à l'url et on attend une seconde que le page soit chargée
     await page.goto(dtUrl);
     await page.waitFor(1000);
 
     const gbData = await page.evaluate(() => {
+
+        // Tableau qui recevra les données
         const gbs = [];
         const gbTitles = document.querySelectorAll('li.row');
+
+        // Pour chaque gb, on récupère le titre, le lien ainsi que la date de post, et on insert ces infos dans le tableau gbs
         gbTitles.forEach(gbElement => {
             try{
                 const gbJson = {
@@ -39,7 +51,9 @@ async function getGbData(){
         return gbs;
     });
 
-    await browser.close();
+    browser.close();
+    
+    // Englobe le résultat entre {} pour en faire un objet Javascript
     return {gbData};
 };
 
@@ -47,11 +61,10 @@ async function getForecast(cityName){
 	const apiKey = 'eaee5ae189087ccabdd90a4d194cbbf4';
     const city = cityName.trim();
     const apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const apiResult = await nodeFetch(apiLink).then(res => {
-        return res.json();
-    });
+    const apiResult = await nodeFetch(apiLink);
+    const weather = await apiResult.json();
 
-    return apiResult;
+    return {weather};
 }
 
 app.get('/', (req, res, next) => {
