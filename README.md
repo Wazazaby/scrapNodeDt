1ca1024f37303520baf2684a7e4d48edba554a5b
Teddy <megareda1998@gmail.com>
2019-11-17 16:40:01 +0100

    Test appel API asynchrone

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # scrap-dt-node
+<pre>
 TP en Node pour scrapper les group-buys sur Deskthority
 Début utilisation de l'API open weather
+</pre>
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express       = require('express');
 const path          = require('path');
 const hogan         = require('hogan-middleware');
 const bodyParser    = require('body-parser');
-const nodeFetch     = require('node-fetch');
+const fetch         = require('node-fetch');
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -14,7 +14,7 @@ const app = express()
     .engine('mustache', hogan.__express);
 
 // Grâce à Pupeteer, récupère les données de la page dans l'url et renvoie un objet Javascript
-async function getGbData(){
+const getGbData = async () => {
     const dtUrl = 'https://deskthority.net/viewforum.php?f=50';
     
     // Lance le browser Chromium ({headless: false} == on voit la page se lancer, passer à true pour que ça se joue en background)
@@ -53,18 +53,26 @@ async function getGbData(){
 
     browser.close();
     
+    
     // Englobe le résultat entre {} pour en faire un objet Javascript
     return {gbData};
 };
 
-async function getForecast(cityName){
+const getForecast = async cityName => {
 	const apiKey = 'eaee5ae189087ccabdd90a4d194cbbf4';
     const city = cityName.trim();
     const apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const apiResult = await nodeFetch(apiLink);
+    const apiResult = await fetch(apiLink);
     const weather = await apiResult.json();
 
     return {weather};
+}
+
+const fetchJsonPlaceholder = async () => {
+    const json = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await json.json();
+
+    return {users};
 }
 
 app.get('/', (req, res, next) => {
@@ -72,11 +80,16 @@ app.get('/', (req, res, next) => {
 }).get('/group-buys', async (req, res, next) => {
     const data = await getGbData();
     res.render('group-buys', data);
-}).get('/forecast', (req, res, next) => {
-    res.render('forecast', null);
-}).post('/forecast', urlencodedParser, async (req, res, next) => {
-    const data = await getForecast(req.body.ville);
-    res.render('forecast', data);
+}).get('/call-json', async (req, res, next) => {
+    const uData = await fetchJsonPlaceholder();
+    res.render('call-json', uData);
 }).listen(8080);
 
 console.log('Server running on port 8080 @ localhost:8080');
+
+// Routes commentés pour OpenWeather
+// }).get('/forecast', (req, res, next) => {
+//     res.render('forecast', null);
+// }).post('/forecast', urlencodedParser, async (req, res, next) => {
+//     const data = await getForecast(req.body.ville);
+//     res.render('forecast', data);
